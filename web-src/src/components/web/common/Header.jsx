5136c377e54df86bb732bf421cb4fa6ed813f3c4
용광순 <gs.yong@hicomp.net>
2023-08-11 12:57:58 +0900

    230811 빌드

--- web-src/src/components/web/common/Header.jsx
+++ web-src/src/components/web/common/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import $ from "jquery";
 import { apiPath, routerPath } from "webPath";
 import { RestServer } from "common/js/Rest";
 import Login from "common/js/Login";
@@ -163,6 +164,109 @@ function Header({ props }) {
     //         signIn(); // Enter 입력이 되면 클릭 이벤트 실행
     //     }
     // };
+
+    $(function () {
+        $("#nav").hide();
+        $(".all_menu").click(function () {
+            $("#nav").slideToggle();
+        });
+    });
+
+    $(function () {
+        $("#nav1_s").hide();
+        $("#nav1").click(function () {
+            $("#nav1_s").slideToggle();
+
+            $("#nav2_s").slideUp();
+            $("#nav3_s").slideUp();
+            $("#nav4_s").slideUp();
+            $("#nav5_s").slideUp();
+            $("#nav6_s").slideUp();
+            $("#nav7_s").slideUp();
+        });
+    });
+
+    $(function () {
+        $("#nav2_s").hide();
+        $("#nav2").click(function () {
+            $("#nav2_s").slideToggle();
+
+            $("#nav1_s").slideUp();
+            $("#nav3_s").slideUp();
+            $("#nav4_s").slideUp();
+            $("#nav5_s").slideUp();
+            $("#nav6_s").slideUp();
+            $("#nav7_s").slideUp();
+        });
+    });
+    $(function () {
+        $("#nav3_s").hide();
+        $("#nav3").click(function () {
+            $("#nav3_s").slideToggle();
+
+            $("#nav1_s").slideUp();
+            $("#nav2_s").slideUp();
+            $("#nav4_s").slideUp();
+            $("#nav5_s").slideUp();
+            $("#nav6_s").slideUp();
+            $("#nav7_s").slideUp();
+        });
+    });
+    $(function () {
+        $("#nav4_s").hide();
+        $("#nav4").click(function () {
+            $("#nav4_s").slideToggle();
+
+            $("#nav1_s").slideUp();
+            $("#nav2_s").slideUp();
+            $("#nav3_s").slideUp();
+            $("#nav5_s").slideUp();
+            $("#nav6_s").slideUp();
+            $("#nav7_s").slideUp();
+        });
+    });
+
+    $(function () {
+        $("#nav5_s").hide();
+        $("#nav5").click(function () {
+            $("#nav5_s").slideToggle();
+
+            $("#nav1_s").slideUp();
+            $("#nav2_s").slideUp();
+            $("#nav3_s").slideUp();
+            $("#nav4_s").slideUp();
+            $("#nav6_s").slideUp();
+            $("#nav7_s").slideUp();
+        });
+    });
+
+    $(function () {
+        $("#nav6_s").hide();
+        $("#nav6").click(function () {
+            $("#nav6_s").slideToggle();
+
+            $("#nav1_s").slideUp();
+            $("#nav2_s").slideUp();
+            $("#nav3_s").slideUp();
+            $("#nav4_s").slideUp();
+            $("#nav5_s").slideUp();
+            $("#nav7_s").slideUp();
+        });
+    });
+
+    $(function () {
+        $("#nav7_s").hide();
+        $("#nav7").click(function () {
+            $("#nav7_s").slideToggle();
+
+            $("#nav1_s").slideUp();
+            $("#nav2_s").slideUp();
+            $("#nav3_s").slideUp();
+            $("#nav4_s").slideUp();
+            $("#nav5_s").slideUp();
+            $("#nav6_s").slideUp();
+        });
+    });
 
     return (
         <>
