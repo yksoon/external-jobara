9f49da32a9e9a5d2e399ec3fe89efe6255149800
용광순 <gs.yong@hicomp.net>
2023-08-14 11:20:11 +0900

    빌드테스트14

--- web-src/src/setupProxy.js
+++ web-src/src/setupProxy.js
@@ -2,9 +2,12 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
     app.use(
-        "/",
+        "/v1",
         createProxyMiddleware({
             target: "http://jejujobara.com:60000",
+            pathRewrite: {
+                "^/v1": "",
+            },
             changeOrigin: true,
         })
     );
