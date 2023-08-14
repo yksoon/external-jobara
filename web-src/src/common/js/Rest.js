5ce98e2d48a2dc37f7fd07f5f6d4f20db83ced35
용광순 <gs.yong@hicomp.net>
2023-08-14 10:01:09 +0900

    빌드테스트8

--- web-src/src/common/js/Rest.js
+++ web-src/src/common/js/Rest.js
@@ -3,22 +3,22 @@ import { Instance, Instance_multi } from "./Instance";
 const RestServer = (method, url, data) => {
     switch (method) {
         case "get":
-            return Instance.get(url, data);
+            return Instance.get(url, data, { withCredentials: true });
 
         case "post":
-            return Instance.post(url, data);
+            return Instance.post(url, data, { withCredentials: true });
 
         case "put":
-            return Instance.put(url, data);
+            return Instance.put(url, data, { withCredentials: true });
 
         case "delete":
-            return Instance.delete(url, data);
+            return Instance.delete(url, data, { withCredentials: true });
 
         case "post_multi":
-            return Instance_multi.post(url, data);
+            return Instance_multi.post(url, data, { withCredentials: true });
 
         case "put_multi":
-            return Instance_multi.put(url, data);
+            return Instance_multi.put(url, data, { withCredentials: true });
         default:
             break;
     }
