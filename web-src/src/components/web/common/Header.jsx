5f4f24ff1405128b4418dca574942f3e0a09d1e9
Eunji <ej.lim@hicomp.net>
2023-08-18 20:42:53 +0900

    mobilenav 추가

--- web-src/src/components/web/common/Header.jsx
+++ web-src/src/components/web/common/Header.jsx
@@ -4,6 +4,7 @@ import $ from "jquery";
 import { apiPath, routerPath } from "webPath";
 import { RestServer } from "common/js/Rest";
 import Login from "common/js/Login";
+import MobileNav from "./MobileNav";
 import { useSelector, useDispatch } from "react-redux";
 import { init_user_info, set_user_info } from "redux/actions/userInfoAction";
 import {
@@ -27,101 +28,12 @@ function Header({ props }) {
         $("#menu-icon2").toggleClass("open");
     };
 
-    $(function () {
-        $("#nav1_s").hide();
-        $("#nav1").click(function () {
-            $("#nav1_s").slideToggle();
+    const menuDepth = (e) => {
+        //$(".nav_2depth").slideToggle();
+        $(".nav_2depth").slideUp();
+        $(e.target).siblings(".nav_2depth").slideToggle();
+    };
 
-            $("#nav2_s").slideUp();
-            $("#nav3_s").slideUp();
-            $("#nav4_s").slideUp();
-            $("#nav5_s").slideUp();
-            $("#nav6_s").slideUp();
-            $("#nav7_s").slideUp();
-        });
-    });
-
-    $(function () {
-        $("#nav2_s").hide();
-        $("#nav2").click(function () {
-            $("#nav2_s").slideToggle();
-
-            $("#nav1_s").slideUp();
-            $("#nav3_s").slideUp();
-            $("#nav4_s").slideUp();
-            $("#nav5_s").slideUp();
-            $("#nav6_s").slideUp();
-            $("#nav7_s").slideUp();
-        });
-    });
-    $(function () {
-        $("#nav3_s").hide();
-        $("#nav3").click(function () {
-            $("#nav3_s").slideToggle();
-
-            $("#nav1_s").slideUp();
-            $("#nav2_s").slideUp();
-            $("#nav4_s").slideUp();
-            $("#nav5_s").slideUp();
-            $("#nav6_s").slideUp();
-            $("#nav7_s").slideUp();
-        });
-    });
-    $(function () {
-        $("#nav4_s").hide();
-        $("#nav4").click(function () {
-            $("#nav4_s").slideToggle();
-
-            $("#nav1_s").slideUp();
-            $("#nav2_s").slideUp();
-            $("#nav3_s").slideUp();
-            $("#nav5_s").slideUp();
-            $("#nav6_s").slideUp();
-            $("#nav7_s").slideUp();
-        });
-    });
-
-    $(function () {
-        $("#nav5_s").hide();
-        $("#nav5").click(function () {
-            $("#nav5_s").slideToggle();
-
-            $("#nav1_s").slideUp();
-            $("#nav2_s").slideUp();
-            $("#nav3_s").slideUp();
-            $("#nav4_s").slideUp();
-            $("#nav6_s").slideUp();
-            $("#nav7_s").slideUp();
-        });
-    });
-
-    $(function () {
-        $("#nav6_s").hide();
-        $("#nav6").click(function () {
-            $("#nav6_s").slideToggle();
-
-            $("#nav1_s").slideUp();
-            $("#nav2_s").slideUp();
-            $("#nav3_s").slideUp();
-            $("#nav4_s").slideUp();
-            $("#nav5_s").slideUp();
-            $("#nav7_s").slideUp();
-        });
-    });
-
-    $(function () {
-        $("#nav7_s").hide();
-        $("#nav7").click(function () {
-            $("#nav7_s").slideToggle();
-
-            $("#nav1_s").slideUp();
-            $("#nav2_s").slideUp();
-            $("#nav3_s").slideUp();
-            $("#nav4_s").slideUp();
-            $("#nav5_s").slideUp();
-            $("#nav6_s").slideUp();
-        });
-    });
 
     // URL 열기
     const openUrl = (url) => {
@@ -138,84 +50,7 @@ function Header({ props }) {
                         </a>
                     </h1>
                     {/* 모바일 메뉴 // S */}
-                    <div id="top_right">
-                        <div
-                            id="menu-icon2"
-                            className="all_menu"
-                            onClick={(e) => menuClick(e)}
-                        >
-                            <span></span>
-                            <span></span>
-                            <span className="short"></span>
-                        </div>
-                        <nav>
-                            <ul id="nav">
-                                <li>
-                                    <Link
-                                        to={routerPath.web_main_url}
-                                        id="nav5"
-                                    >
-                                        홈
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        to={routerPath.web_intro_url}
-                                        id="nav1"
-                                    >
-                                        박람회안내
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link 
-                                        to={routerPath.web_program_url}
-                                        id="nav2">
-                                        프로그램
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        to={routerPath.web_signup_url}
-                                        id="nav3"
-                                    >
-                                        사전등록
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        to={routerPath.web_signupchk_url}
-                                        id="nav3"
-                                    >
-                                        사전등록 확인
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        onClick={() =>
-                                            openUrl(
-                                                "https://lincplus.jejunu.ac.kr/programs/notice.htm?act=view&seq=1364"
-                                            )
-                                        }
-                                        id="nav4"
-                                    >
-                                        참여기업
-                                    </Link>
-                                </li>
-                                {/* <li>
-                                    <Link href="" id="nav5">
-                                        공지
-                                    </Link>
-                                </li> */}
-                            </ul>
-                        </nav>
-                    </div>
-                    {/* <script>				
-                    $(document).ready(function(){
-                        $('#menu-icon2').click(function(){
-                            $(this).toggleClass('open');
-                        });
-                    });
-                </script> */}
+                    <MobileNav />
                     {/* 모바일메뉴 // E */}
                 </div>
             </div>
---
+++ web-src/src/components/web/common/MobileNav.jsx
@@ -0,0 +1,126 @@
+import React, { useEffect, useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import $ from "jquery";
+import { apiPath, routerPath } from "webPath";
+import { RestServer } from "common/js/Rest";
+import Login from "common/js/Login";
+import { useSelector, useDispatch } from "react-redux";
+import { init_user_info, set_user_info } from "redux/actions/userInfoAction";
+import {
+    CommonConsole,
+    CommonErrorCatch,
+    CommonNotify,
+} from "common/js/Common";
+import { set_spinner } from "redux/actions/commonAction";
+import useAlert from "hook/useAlert";
+
+let resultCode;
+let loginInfo;
+
+function MobileNav({ props }) {
+    useEffect(() => {
+        $("#nav").hide();
+    }, []);
+
+    const menuClick = () => {
+        $("#nav").slideToggle();
+        $("#menu-icon2").toggleClass("open");
+    };
+
+    const menuDepth = (e) => {
+        $(".nav_2depth").slideUp();
+        $(e.target).siblings(".nav_2depth").slideToggle();
+    };
+
+
+    // URL 열기
+    const openUrl = (url) => {
+        window.open(url, "_blank", "noopener, noreferrer");
+    };
+
+    return (
+        <>
+            {/* 모바일 메뉴 // S */}
+            <div id="top_right">
+                <div
+                    id="menu-icon2"
+                    className="all_menu"
+                    onClick={(e) => menuClick(e)}
+                >
+                    <span></span>
+                    <span></span>
+                    <span className="short"></span>
+                </div>
+                <nav>
+                    <ul id="nav">
+                        <li>
+                            <Link
+                                to={routerPath.web_main_url}
+                                id="nav5"
+                            >
+                                홈
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                id="nav1"
+                                onClick={(e) => menuDepth(e)}
+                            >
+                                박람회안내
+                            </Link>
+                            <ul className="nav_2depth">
+                                <li><Link to={routerPath.web_intro_url}>행사소개</Link></li>
+                                <li><Link to={routerPath.web_Location_url}>행사장소</Link></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <Link 
+                                id="nav2"
+                                onClick={(e) => menuDepth(e)}
+                                >
+                                프로그램
+                            </Link>
+                            <ul className="nav_2depth">
+                                <li><Link to={routerPath.web_program_url}>행사일정</Link></li>
+                                <li><Link  to={routerPath.web_program_detail_url}>세부프로그램</Link></li>
+                                <li><Link  to={routerPath.web_program_event_url}>이벤트프로그램</Link></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <Link
+                                id="nav3"
+                                onClick={(e) => menuDepth(e)}
+                            >
+                                사전등록
+                            </Link>
+                            <ul className="nav_2depth">
+                                <li><Link to={routerPath.web_signup_url}>사전등록</Link></li>
+                                <li><Link to={routerPath.web_signupchk_url}>사전등록확인</Link></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <Link
+                                onClick={() =>
+                                    openUrl(
+                                        "https://lincplus.jejunu.ac.kr/programs/notice.htm?act=view&seq=1364"
+                                    )
+                                }
+                                id="nav4"
+                            >
+                                참여기업
+                            </Link>
+                        </li>
+                        {/* <li>
+                            <Link href="" id="nav5">
+                                공지
+                            </Link>
+                        </li> */}
+                    </ul>
+                </nav>
+            </div>
+            {/* 모바일메뉴 // E */}
+        </>
+    );
+}
+
+export default MobileNav;
