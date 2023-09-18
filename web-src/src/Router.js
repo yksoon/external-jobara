be0e3c35dc15d16f29c541be4846c8d96895840c
용광순 <gs.yong@hicomp.net>
2023-09-18 09:25:14 +0900

    잡아라 종료 날짜 라우팅 설정

--- web-src/src/Router.js
+++ web-src/src/Router.js
@@ -17,12 +17,16 @@ import Detail from "components/web/program/Detail";
 import Event from "components/web/program/Event";
 import Notice from "components/web/notice/Notice";
 import Company from "components/web/company/Company";
+import Forbidden from "Forbidden";
 
 // Router
 const Router = () => {
     // 레이지 로딩 추가
     const Main = React.lazy(() => import("components/web/Main"));
     const SignUp = React.lazy(() => import("components/web/signUp/SignUp"));
+
+    const nowDate = new Date();
+    const endDate = new Date("2023-09-30 23:59:59");
 
     // 페이지 url 라우팅 추가 필요시 아래에 추가하세요
     return (
@@ -41,121 +45,130 @@ const Router = () => {
                     </Backdrop>
                 }
             >
-                <Routes>
-                    {/* /link를 입력하면 LinkPage 오픈 */}
-                    {/* -------------------------------web------------------------------- */}
-                    {/* 메인 */}
-                    {/* URL : / */}
-                    <Route path={routerPath.web_main_url} element={<Main />} />
+                {nowDate < endDate ? (
+                    <Routes>
+                        {/* /link를 입력하면 LinkPage 오픈 */}
+                        {/* -------------------------------web------------------------------- */}
+                        {/* 메인 */}
+                        {/* URL : / */}
+                        <Route
+                            path={routerPath.web_main_url}
+                            element={<Main />}
+                        />
 
-                    {/* 행사소개 */}
-                    {/* URL : /intro/intro */}
-                    <Route
-                        path={routerPath.web_intro_url}
-                        element={<Intro />}
-                    />
+                        {/* 행사소개 */}
+                        {/* URL : /intro/intro */}
+                        <Route
+                            path={routerPath.web_intro_url}
+                            element={<Intro />}
+                        />
 
-                    {/* 행사장소 */}
-                    {/* URL : /intro/location */}
-                    <Route
-                        path={routerPath.web_intro_location_url}
-                        element={<Location />}
-                    />
+                        {/* 행사장소 */}
+                        {/* URL : /intro/location */}
+                        <Route
+                            path={routerPath.web_intro_location_url}
+                            element={<Location />}
+                        />
 
-                    {/* 프로그램 */}
-                    {/* URL : /program/program */}
-                    <Route
-                        path={routerPath.web_program_url}
-                        element={<Program />}
-                    />
-                    {/* 프로그램 */}
-                    {/* URL : /program/detail */}
-                    <Route
-                        path={routerPath.web_program_detail_url}
-                        element={<Detail />}
-                    />
+                        {/* 프로그램 */}
+                        {/* URL : /program/program */}
+                        <Route
+                            path={routerPath.web_program_url}
+                            element={<Program />}
+                        />
+                        {/* 프로그램 */}
+                        {/* URL : /program/detail */}
+                        <Route
+                            path={routerPath.web_program_detail_url}
+                            element={<Detail />}
+                        />
 
-                    {/* 프로그램 */}
-                    {/* URL : /program/event */}
-                    <Route
-                        path={routerPath.web_program_event_url}
-                        element={<Event />}
-                    />
+                        {/* 프로그램 */}
+                        {/* URL : /program/event */}
+                        <Route
+                            path={routerPath.web_program_event_url}
+                            element={<Event />}
+                        />
 
-                    {/* 공지사항 */}
-                    {/* URL : /notice/notice */}
-                    <Route
-                        path={routerPath.web_notice_url}
-                        element={<Notice />}
-                    />
+                        {/* 공지사항 */}
+                        {/* URL : /notice/notice */}
+                        <Route
+                            path={routerPath.web_notice_url}
+                            element={<Notice />}
+                        />
 
-                    {/* 사전등록 */}
-                    {/* URL : /signup */}
-                    <Route
-                        path={routerPath.web_signup_url}
-                        element={<SignUp />}
-                    />
+                        {/* 사전등록 */}
+                        {/* URL : /signup */}
+                        <Route
+                            path={routerPath.web_signup_url}
+                            element={<SignUp />}
+                        />
 
-                    {/* 사전등록 확인 */}
-                    {/* URL : /signupchk */}
-                    <Route
-                        path={routerPath.web_signupchk_url}
-                        element={<SignUpChk />}
-                    />
+                        {/* 사전등록 확인 */}
+                        {/* URL : /signupchk */}
+                        <Route
+                            path={routerPath.web_signupchk_url}
+                            element={<SignUpChk />}
+                        />
 
-                    {/* 사전등록 수정 */}
-                    {/* URL : /signup_mod */}
-                    <Route
-                        path={routerPath.web_signup_mod_url}
-                        element={<SignUpMod />}
-                    />
+                        {/* 사전등록 수정 */}
+                        {/* URL : /signup_mod */}
+                        <Route
+                            path={routerPath.web_signup_mod_url}
+                            element={<SignUpMod />}
+                        />
 
-                    {/* 공지사항 */}
-                    {/* URL : /notice */}
-                    <Route
-                        path={routerPath.web_signup_mod_url}
-                        element={<SignUpMod />}
-                    />
+                        {/* 공지사항 */}
+                        {/* URL : /notice */}
+                        <Route
+                            path={routerPath.web_signup_mod_url}
+                            element={<SignUpMod />}
+                        />
 
-                    {/* 참여기업 */}
-                    {/* URL : /company/company/ */}
-                    <Route
-                        path={`${routerPath.web_company_url}/:companyName`}
-                        element={<Company />}
-                    />
+                        {/* 참여기업 */}
+                        {/* URL : /company/company/ */}
+                        <Route
+                            path={`${routerPath.web_company_url}/:companyName`}
+                            element={<Company />}
+                        />
 
-                    {/* 참여기업 리스트 */}
-                    {/* URL : /company/companylist */}
-                    {/* <Route
+                        {/* 참여기업 리스트 */}
+                        {/* URL : /company/companylist */}
+                        {/* <Route
                         path={routerPath.web_company_list_url}
                         element={<CompanyList />}
                     /> */}
 
-                    {/* 참여기업 상세 */}
-                    {/* URL : /company/companylist */}
-                    {/* <Route
+                        {/* 참여기업 상세 */}
+                        {/* URL : /company/companylist */}
+                        {/* <Route
                         path={`${routerPath.web_company_detail_url}`}
                         element={<CompanyDetail />}
                     /> */}
 
-                    {/* -------------------------------admin------------------------------- */}
-                    {/* 메인 */}
-                    {/* URL : /admin */}
-                    <Route
-                        path={routerPath.admin_main_url}
-                        element={<Admin />}
-                    />
+                        {/* -------------------------------admin------------------------------- */}
+                        {/* 메인 */}
+                        {/* URL : /admin */}
+                        <Route
+                            path={routerPath.admin_main_url}
+                            element={<Admin />}
+                        />
 
-                    {/* 로그인 */}
-                    {/* URL : /admin/signin */}
-                    <Route
-                        path={routerPath.admin_signin_url}
-                        element={<SignIn />}
-                    />
+                        {/* 로그인 */}
+                        {/* URL : /admin/signin */}
+                        <Route
+                            path={routerPath.admin_signin_url}
+                            element={<SignIn />}
+                        />
 
-                    {/* 404 */}
-                    <Route path="*" element={<NotFoundPage />} />
-                </Routes>
+                        {/* 404 */}
+                        <Route path="*" element={<NotFoundPage />} />
+                    </Routes>
+                ) : (
+                    <Routes>
+                        <Route path="*" element={<Forbidden />} />
+                    </Routes>
+                )}
             </Suspense>
         </>
     );
