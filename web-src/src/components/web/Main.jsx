bdb134bbaff816a30931151a64ff1d85d08e9ca3
용광순 <gs.yong@hicomp.net>
2023-09-19 16:15:31 +0900

    9.20 이벤트 발표 메인 모달 팝업 구현/빌드

--- web-src/src/components/web/Main.jsx
+++ web-src/src/components/web/Main.jsx
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from "react";
+import { React, useEffect, useRef, useState } from "react";
 
 import Header from "components/web/common/Header";
 import Footer from "components/web/common/Footer";
@@ -12,10 +12,51 @@ function Main() {
         Aos.init();
     });
 
-    const [isOpen, setIsOpen] = useState(true);
+    const popupTime = localStorage.getItem("popupTime");
+    const [isOpen, setIsOpen] = useState(false);
 
+    const checkBoxRef = useRef(null);
+
+    useEffect(() => {
+        popupOpenTime();
+    }, []);
+
+    // 메인 팝업 시간
+    const popupOpenTime = () => {
+        const nowDate = new Date();
+        const openDate = new Date("2023-09-20 00:00:00");
+
+        if (nowDate > openDate) {
+            const today = new Date();
+
+            if (popupTime && popupTime > today) {
+                // 현재 date가 localStorage의 시간보다 크면 return
+                setIsOpen(false);
+                return;
+            }
+            if (!popupTime || popupTime < today) {
+                // 저장된 date가 없거나 today보다 작다면 popup 노출
+                setIsOpen(true);
+            }
+        } else {
+            setIsOpen(false);
+        }
+    };
+
+    // 팝업 닫기 핸들러
     const handleModalClose = () => {
-        setIsOpen(false);
+        const checked = checkBoxRef.current.checked;
+        if (checked) {
+            let expires = new Date();
+            expires = expires.setHours(expires.getHours() + 24);
+
+            localStorage.setItem("popupTime", expires);
+
+            setIsOpen(false);
+        } else {
+            setIsOpen(false);
+        }
+        // console.log(checkBoxRef.current.checked);
     };
 
     return (
@@ -38,6 +79,7 @@ function Main() {
                 title={""}
                 width={"1000"}
                 handleModalClose={handleModalClose}
+                ref={checkBoxRef}
             />
         </>
     );
